d17f9a617145fe7cbd7c1d615c85e494a2ae8bfb
jonah-tech <kiharajoanh@gmail.com>
2022-01-14 11:34:15 +0300

    initial commit

---
+++ src/urlConfig.js
@@ -0,0 +1,7 @@
+const baseUrl = process.env.API || "http://localhost:2000";
+//const baseUrl = "http://localhost:2000";
+export const api = `${baseUrl}/api`;
+
+export const generatePublicUrl = (fileName) => {
+  return `${baseUrl}/public/${fileName}`;
+};
